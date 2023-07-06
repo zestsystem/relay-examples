d069ab3d63a21bf3c75dc81240578e765b1a2564
Mike Yim <mikeyim@unitetheculture.org>
2023-07-06 15:46:59 -0700

    tutorial: refetchable fragments

--- newsfeed/src/components/ContactsList.tsx
+++ newsfeed/src/components/ContactsList.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
+import { useState, useTransition } from "react";
 import { graphql } from "relay-runtime";
-import { useFragment } from "react-relay";
+import { useRefetchableFragment } from "react-relay";
 import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 import Card from "./Card";
 import ContactRow from "./ContactRow";
+import SearchInput from "./SearchInput";
 
 export type Props = {
-  viewer: ContactsListFragment$key;
+    viewer: ContactsListFragment$key;
 };
 
 const ContactsListFragment = graphql`
-  fragment ContactsListFragment on Viewer {
-    contacts {
+  fragment ContactsListFragment on Viewer 
+    @refetchable(queryName: "ContactsListRefetchQuery")
+    @argumentDefinitions(
+      search: { type: "String", defaultValue: null }
+    )
+    {
+    contacts(search: $search) {
       id
       ...ContactRowFragment
     }
@@ -19,13 +26,22 @@ const ContactsListFragment = graphql`
 `;
 
 export default function ContactsList({ viewer }: Props) {
-  const data = useFragment(ContactsListFragment, viewer);
-  return (
-    <Card dim={true}>
-      <h3>Contacts</h3>
-      {data.contacts.map((contact) => (
-        <ContactRow key={contact.id} contact={contact} />
-      ))}
-    </Card>
-  );
+    const [isPending, startTransition] = useTransition();
+    const [data, refetch] = useRefetchableFragment(ContactsListFragment, viewer);
+    const [searchString, setSearchString] = useState('');
+    const onSearchStringChanged = (value: string) => {
+        setSearchString(value);
+        startTransition(() => {
+            refetch({ search: value });
+        });
+    };
+    return (
+        <Card dim={true}>
+            <h3>Contacts</h3>
+            <SearchInput value={searchString} onChange={onSearchStringChanged} isPending={isPending} />
+            {data.contacts.map((contact) => (
+                <ContactRow key={contact.id} contact={contact} />
+            ))}
+        </Card>
+    );
 }
